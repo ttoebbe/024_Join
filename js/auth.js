9173455ef63b6d890926991794ccf6a012d6e4e5
Fernando <fernando13pa@hotmail.com>
2026-01-10 11:36:27 +0100

    feat icon lock

--- js/auth.js
+++ js/auth.js
@@ -47,46 +47,46 @@ function startAnimation() {
 }
 
 /* ================== PASSWORD TOGGLE ================== */
- function setupPasswordToggle(inputId, lockId, eyeId) {
-    const input = document.getElementById(inputId);
-    const lock = document.getElementById(lockId);
-    const eye = document.getElementById(eyeId);
+function setupPasswordToggle(inputId, lockId, eyeId) {
+  const input = document.getElementById(inputId);
+  const lock = document.getElementById(lockId);
+  const eye = document.getElementById(eyeId);
 
-    if (!input || !lock || !eye) return;
+  if (!input || !lock || !eye) return;
 
-    // Initialzustand
-    eye.classList.add("d-none");
-    lock.classList.remove("d-none");
-    input.type = "password";
+  // Initialzustand
+  eye.classList.add("d-none");
+  lock.classList.remove("d-none");
+  input.type = "password";
 
-    // 👇 Eye erst anzeigen, wenn etwas eingegeben wurde
-    input.addEventListener("input", () => {
-      if (input.value.length > 0) {
-        lock.classList.add("d-none");
-        eye.classList.remove("d-none");
-        eye.src = "../img/icons/visibility_off.png";
-      } else {
-        lock.classList.remove("d-none");
-        eye.classList.add("d-none");
-        input.type = "password";
-      }
-    });
+  // 👇 Eye erst anzeigen, wenn etwas eingegeben wurde
+  input.addEventListener("input", () => {
+    if (input.value.length > 0) {
+      lock.classList.add("d-none");
+      eye.classList.remove("d-none");
+      eye.src = "../img/icons/visibility_off.png";
+    } else {
+      lock.classList.remove("d-none");
+      eye.classList.add("d-none");
+      input.type = "password";
+    }
+  });
 
-    // 👁 Toggle nur wenn Eye sichtbar ist
-    eye.addEventListener("click", (e) => {
-      e.stopPropagation();
+  // 👁 Toggle nur wenn Eye sichtbar ist
+  eye.addEventListener("click", (e) => {
+    e.stopPropagation();
 
-      if (input.type === "password") {
-        input.type = "text";
-        eye.src = "../img/icons/visibility.png";
-      } else {
-        input.type = "password";
-        eye.src = "../img/icons/visibility_off.png";
-      }
-    });
+    if (input.type === "password") {
+      input.type = "text";
+      eye.src = "../img/icons/visibility.png";
+    } else {
+      input.type = "password";
+      eye.src = "../img/icons/visibility_off.png";
+    }
+  });
 
-    lock.addEventListener("click", (e) => e.stopPropagation());
-  }
+  lock.addEventListener("click", (e) => e.stopPropagation());
+}
 
 /* ================== LOGIN ================== */
 function initLogin() {
@@ -133,6 +133,7 @@ function initLogin() {
   pwEl.addEventListener("input", updateBtn);
   updateBtn();
 
+  setupPasswordToggle("suPw", "lockPw", "eyePw");
   // Falls du auch im Login Eye/Lock hast, IDs hier eintragen:
   // setupPasswordToggle("password", "lockPwLogin", "eyePwLogin");
 }
